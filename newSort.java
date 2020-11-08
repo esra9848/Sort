b90e3f63201739b2ae013567ab4cf0e218eeb76d
esra9848 <esraulgen@eskisehir.edu.tr>
2020-11-08 12:43:13 +0300

    my first repestory

---
+++ newSort.java
@@ -0,0 +1,24 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Collections;
+import java.util.List;
+
+public class newSort {
+	public static void main(String[] args) throws IOException {
+		if(args.length!=1) {
+			System.out.println("plase supply an argument : file name ");
+			
+		}
+		Path mypath = Paths.get(args[0]);
+
+		List<String> lines = Files.readAllLines(mypath);
+		Collections.sort(lines);
+		
+		for (String s : lines) {
+			System.out.println(s);
+		}
+	}
+
+}
